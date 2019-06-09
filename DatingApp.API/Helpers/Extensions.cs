b37abf11172b9aa522847fef9ec8e46ddaa32daf
Aaron Alanis <alanis265681@gmail.com>
2019-06-09 00:08:01 -0500

    end of 8

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Http;
 
 namespace DatingApp.API.Helpers
@@ -7,8 +8,15 @@ namespace DatingApp.API.Helpers
         public static void AddApplicationError(this HttpResponse response, string message)
         {
             response.Headers.Add("Application-Error", message);
-            response.Headers.Add("Accsess-Control-Expose-Headers","Application-Error");
-            response.Headers.Add("Accsess-Control-Allow-Origin","*");
+            response.Headers.Add("Accsess-Control-Expose-Headers", "Application-Error");
+            response.Headers.Add("Accsess-Control-Allow-Origin", "*");
+        }
+        public static int CalculateAge(this DateTime theDateTime)
+        {
+            var age = DateTime.Today.Year - theDateTime.Year;
+            if (theDateTime.AddYears(age) > DateTime.Today)
+                age--;
+            return 1;
         }
     }
 }
